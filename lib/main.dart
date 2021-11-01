2134afef72eb11cacd8e11ff226dc8ecaea2fd66
KIM-JUHYEON-79 <jennykim6065@naver.com>
2021-11-01 23:27:10 +0900

    top_bar

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'together',//상단바 제목작성
+      theme:ThemeData(
+          primarySwatch: Colors.blue
+      ),
+      home : MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatefulWidget{
+  @override
+  State<MyPage> createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+
+        appBar: AppBar(
+        title: Text('together'),
+        centerTitle: false, elevation:0.0,
+
+        leading: IconButton(
+          icon:Icon(Icons.arrow_left_rounded),
+          onPressed: (){
+            print('gotoback(navigation위젯');
+          }
+
+  ),
+    // actions: [
+    //   IconButton(
+    //     icon: Icon(
+    //       Icons.account_circle,
+    //     ),
+    //     onPressed: (){},
+    //   ),
+    // ],
+  ),
+
+      endDrawer : Drawer(
+        child : ListView(
+          padding : EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child:Text('Drawer'),
+                decoration: BoxDecoration(
+                  color:Colors.blue,),
+                  ),
+                ],
+              )
+            ),
+  );
+}
+}
