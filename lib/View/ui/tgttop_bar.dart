894cdf279532a328e96d1fad3fb467938e7dfa03
김주현 <72371540+KIM-JUHYEON-79@users.noreply.github.com>
2021-11-15 18:53:00 +0900

    tgttop_bar 수정

--- lib/View/ui/tgttop_bar.dart
+++ lib/View/ui/tgttop_bar.dart
@@ -1,78 +1,42 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TGTtop_bar extends StatefulWidget{
+class TGTtop_bar extends StatelessWidget implements PreferredSizeWidget {
 
-  var title = "together";
-  final VoidCallback onPressed;
-
-  TGTtop_bar({
-    Key? key,
+  TGTtop_bar({required Key key,
+    required this.appBar,
     required this.title,
-    required this.onPressed,
-  }) : super(key: key);
+    this.center = true})
+      : super(key: key);
 
-  @override
-  _TGTtop_bar createState() => _TGTtop_bar();
-}
-
-class _TGTtop_bar extends State<TGTtop_bar> {
-  var title;
+  final AppBar appBar;
+  final String title;
+  final bool center;
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
 
   @override
-
-  void initState(){
-    super.initState();
-  }
-  Widget build(BuildContext context){
-    return Scaffold(
+  Widget build(BuildContext context) {
+    return AppBar(
       key:_scaffoldKey,
-      endDrawer : Container(
-        width:250,
-        child : Drawer(
-            child : ListView(
-              padding : EdgeInsets.zero,
-              children: <Widget>[
-                DrawerHeader(
-                  child:Text('Drawer'),
-                  decoration: BoxDecoration(
-                    color:Colors.blue,),
-                ),
-              ],
-            )
+        title: Text("title"),
+        centerTitle: center,
+        leading: IconButton(
+           icon:Icon(Icons.arrow_left_rounded),
+             onPressed: () => Navigator.of(context).pop(),
         ),
-      ),
-      appBar: AppBar(
-          title: Text('together'),
-          centerTitle: true, elevation:0.0,
-          leading: IconButton(
-              icon:Icon(Icons.arrow_left_rounded),
-              onPressed: (){
-                print('gotoback(navigation위젯');
-              }
+      actions: <Widget>[
+        Container(
+          child: IconButton(
+          iconSize: 40,
+           color: Colors.black,
+           icon: Icon(Icons.menu),
+            onPressed: () =>
+              _scaffoldKey.currentState!.openEndDrawer(),
           ),
-          actions: <Widget>[
-            Container(
-              child: IconButton(
-                iconSize: 40,
-                color: Colors.black,
-                icon: Icon(Icons.menu),
-                onPressed: () {
-                  _scaffoldKey.currentState!.openEndDrawer();
-                },
-              ),
-            )
-          ]),
-      // actions: [
-      //   IconButton(
-      //     icon: Icon(
-      //       Icons.account_circle,
-      //     ),
-      //     onPressed: (){},
-      //   ),
-      // ],
+    ),
+      ]
     );
   }
-
+  @override
+  Size get preferredSize => Size.fromHeight(appBar.preferredSize.height);
 }
