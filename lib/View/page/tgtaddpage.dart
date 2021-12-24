a2cb8a6d24c47b72952ced6faa7354af9d46063a
KIM-JUHYEON-79 <jennykim6065@naver.com>
2021-12-25 02:04:21 +0900

    addpage modi

--- lib/View/page/tgtaddpage.dart
+++ lib/View/page/tgtaddpage.dart
@@ -6,6 +6,7 @@ import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:togetor/Controller/GoogleMapService/map_bloc.dart';
+import 'package:togetor/View/ui/tgtbutton.dart';
 import '../ui/tgt_side_bar.dart';
 import '../ui/tgttop_bar.dart';
 import '../page/tgt_placepicker_page.dart';
@@ -30,31 +31,33 @@ class Add extends StatefulWidget {
 }
 
 class _AddState extends State<Add> {
-  File? image;
+  File? _contentImage;
 
   Future<File> saveImagePermanently(String imagePath) async {
     final directory = await getApplicationDocumentsDirectory();
-    final image = File('${directory.path}/name');
-
-    return File(imagePath).copy(image.path);
+    final _contentImage = File('${directory.path}/name');
+    return File(imagePath).copy(_contentImage.path);
   }
 
+  final TextEditingController _title = new TextEditingController();
+  final TextEditingController _detail = new TextEditingController();
   final _valueList = ['FOOD', 'PRACTICE', 'GOODS', 'ETC'];
   var _selectedValue;
-
-  late CountProvider _counterProvider;
+  //_place 추가하기
+  late DateTime _limitedDate = new DateTime.now();
+  late CountProvider _maxparticipant;
   final key2 = const Key('key2');
 
   @override
   Widget build(BuildContext context) {
-    _counterProvider = Provider.of<CountProvider>(context);
+    _maxparticipant = Provider.of<CountProvider>(context);
 
     Future pickImage(ImageSource source) async {
       try {
-        final image = await ImagePicker().pickImage(source: source);
-        if (image == null) return;
-        final imagePermanent = await saveImagePermanently(image.path);
-        setState(() => this.image = imagePermanent);
+        final _contentImage = await ImagePicker().pickImage(source: source);
+        if (_contentImage == null) return;
+        final imagePermanent = await saveImagePermanently(_contentImage.path);
+        setState(() => this._contentImage = imagePermanent);
       } on PlatformException catch (e) {
         print('Failed to pick Image : $e');
       }
@@ -104,16 +107,21 @@ class _AddState extends State<Add> {
       body: SafeArea(
           child: Column(
             children: <Widget>[
-              image != null
+              _contentImage != null
                   ? Container(
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: FileImage(image!)
-                      )
-                   )
-
+                width: 100, height: 100,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: FileImage(File(_contentImage!.path))
+                  ),
+                ),
               )
-                  : RaisedButton(onPressed: () =>
+                  : IconButton(
+                icon : Icon(Icons.add_photo_alternate),
+                color: Colors.grey,
+                iconSize: 100,
+                onPressed: () {
                   showDialog<void>(
                       context: context,
                       builder: (BuildContext context) {
@@ -126,13 +134,14 @@ class _AddState extends State<Add> {
                             fontWeight: FontWeight.bold,
                           ),
                           ),
-
                           children: <Widget>[
                             SimpleDialogOption(
                               child: Text('사진 찍기',
                                   style: TextStyle(color: Colors.black)),
-                              onPressed: () => pickImage(ImageSource.camera),
+                              onPressed : () => pickImage(ImageSource.camera),
                             ),
+
+
                             SimpleDialogOption(
                               child: Text('사진 가져오기',
                                   style: TextStyle(color: Colors.black)),
@@ -143,25 +152,26 @@ class _AddState extends State<Add> {
                                   style: TextStyle(color: Colors.grey)),
                               onPressed: () => Navigator.pop(context),
                             ),
-
                           ],
                         );
                       }
-                  ),
-                  child: Icon(Icons.add_photo_alternate, color: Colors.grey,
-                      size: 100)),
+                  ); },
+              ),
 
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child:TextFormField(
+                child: TextFormField(
+                  controller : _title,
                   decoration: InputDecoration(
                       labelText: '제목을 입력해주세요',
-                      border: OutlineInputBorder()),
+                      border: OutlineInputBorder()
+                  ),
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child:TextFormField(
+                child: TextFormField(
+                  controller: _detail,
                   decoration: InputDecoration(
                       labelText: '상세내용을 입력해주세요',
                       border: OutlineInputBorder()),
@@ -172,12 +182,12 @@ class _AddState extends State<Add> {
                 child: Center(
                   child: DropdownButton(
                     hint: Text('카테고리'),
-                    value : _selectedValue,
-                    items: _valueList.map((value){
-                          return DropdownMenuItem(
-                            value : value, child: Text(value));
-                        }).toList(),
-                    onChanged: (value){
+                    value: _selectedValue,
+                    items: _valueList.map((value) {
+                      return DropdownMenuItem(
+                          value: value, child: Text(value));
+                    }).toList(),
+                    onChanged: (value) {
                       print(value);
                       setState(() {
                         _selectedValue = value;
@@ -186,49 +196,80 @@ class _AddState extends State<Add> {
                   ),
                 ),
               ),
-              Padding(
-                padding:const EdgeInsets.all(8.0),
-                child: TextButton(
-                  onPressed: (){
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (BuildContext context) => MapScreen()));
-                  },
-                  child: Text("위치 선택"),
-                ),
+              TextButton(
+                child: Text('위치선택'),
+                onPressed: (){},
+              ),
+              TextButton(
+                child: Text('날짜선택'),
+                onPressed: (){
+                  Future<DateTime?> selectedDate = showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2021),
+                    lastDate: DateTime(2025),
+                    builder: (BuildContext context, Widget? child){
+                      return Theme(
+                        data: ThemeData.light(),
+                        child: child!,
+                      );
+                    },
+                  );
+                  selectedDate.then((dateTime){
+                    setState((){
+                      _limitedDate = dateTime!;
+                    });
+                  });
+                },
+              ),
+              Text('${_limitedDate.year}/${_limitedDate.month}/${_limitedDate.day}', style: TextStyle(fontSize: 20),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
-
                 children: [
-
                   Text('인원수'),
                   Text(Provider.of<CountProvider>(context).count.toString()),
-                  IconButton(onPressed:(){_counterProvider.add();},
-                      icon: Icon(Icons.add),
-                      ),
-                  IconButton(onPressed:(){_counterProvider.remove();},
-                      icon: Icon(Icons.remove),
+                  IconButton(onPressed: () {
+                    _maxparticipant.add();
+                  },
+                    icon: Icon(Icons.add),
+                  ),
+                  IconButton(onPressed: () {
+                    _maxparticipant.remove();
+                  },
+                    icon: Icon(Icons.remove),
                   ),
                 ],
               ),
-              Padding(
-                padding:const EdgeInsets.all(8.0),
-                child: TextButton(
-                  onPressed: (){
-                    print("test");
-                  },
-                  child: Text("설정"),
+              TGTButton(
+                child: const Icon(
+                  Icons.upload,
+                  color: Colors.white,
+                  size: 30.0,
                 ),
-              ),
+                onPressed: () {
+                  //setContent
+                },
+              )
             ],
           )
-
       ),
-
     );
-
   }
-
-
-
 }
+
+// void setContent() async {
+//   String TGTContenturl = "http://118.223.255.68:4000/";
+//   var TGTContentresponse = await http.get(TGTContenturl);
+//   var TGTContentData = convert.jsonDecode(
+//       convert.utf8.decode(TGTContentresponse.bodyBytes));
+//   setState(
+//         () {
+//       _title = TGTContentData["title"];
+//       _detail = TGTContentData["detail"];
+//       _contentImage = TGTContentData["contentImage"];
+//       _maxParticipant = TGTContentData["maxParticipant"];
+//       _limitedDate = TGTContentData["limitedDate"];
+//     },
+//   );
+// }
