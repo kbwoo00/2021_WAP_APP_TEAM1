5c766c79a4392147679871cff4c3c433ba702737
KIMBYUNGWOO <59406944+kbwoo00@users.noreply.github.com>
2021-12-26 23:25:00 +0900

    place_picker 페이지 보완(데이터 공유 전)

--- lib/View/page/tgt_placepicker_page.dart
+++ lib/View/page/tgt_placepicker_page.dart
@@ -2,17 +2,19 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:togetor/View/page/tgtaddpage.dart';
 import '../../Model/PlacePicker/place.dart';
 import '../../Controller/GoogleMapService/map_bloc.dart';
 import '../../Controller/GoogleMapService/place_service.dart';
 
-
 class MapScreen extends StatelessWidget {
+  const MapScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => MapBloc(),
-      child: MaterialApp(
+      child: const MaterialApp(
         home: MapSample(),
       ),
     );
@@ -27,31 +29,27 @@ class MapSample extends StatefulWidget {
 }
 
 class _MapSampleState extends State<MapSample> {
-  Completer<GoogleMapController> _mapController = Completer();
+  final Completer<GoogleMapController> _mapController = Completer();
   late StreamSubscription locationSubscription;
-  var place = null;
 
   final placeService = PlaceService();
   final Set<Marker> _markers = Set();
+  var place;
 
   @override
-  void initState() {
-    final applicationBloc =
-    Provider.of<MapBloc>(context, listen: false);
+  void initState() async{
+    final applicationBloc = Provider.of<MapBloc>(context, listen: false);
 
     locationSubscription =
         applicationBloc.selectedLocation.stream.listen((place) {
-          if (place != null) {
-            _goToPlace(place);
-          }
-        });
+      _goToPlace(place);
+    });
     super.initState();
   }
 
   @override
   void dispose() {
-    final applicationBloc =
-    Provider.of<MapBloc>(context, listen: false);
+    final applicationBloc = Provider.of<MapBloc>(context, listen: false);
     applicationBloc.dispose();
     locationSubscription.cancel();
     super.dispose();
@@ -60,123 +58,155 @@ class _MapSampleState extends State<MapSample> {
   @override
   Widget build(BuildContext context) {
     final applicationBloc = Provider.of<MapBloc>(context);
-    return Scaffold(
-      body: (applicationBloc.currentLocation == null)
-          ? Center(
-        child: CircularProgressIndicator(),
-      )
-          : ListView(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Search Location',
-                suffixIcon: Icon(Icons.search),
-              ),
-              onChanged: (value) => applicationBloc.searchPlaces(value),
-            ),
-          ),
-          Stack(
-            children: [
-              Container(
-                height: (MediaQuery.of(context).size.height) * 0.6,
-                child: GoogleMap(
-                  myLocationEnabled: true,
-                  initialCameraPosition: CameraPosition(
-                      target: LatLng(
-                          applicationBloc.currentLocation!.latitude,
-                          applicationBloc.currentLocation!.longitude),
-                      zoom: 14),
-                  onMapCreated: (GoogleMapController controller) {
-                    _mapController.complete(controller);
-                  },
-                  markers: _markers,
-                ),
-              ),
-              if (applicationBloc.searchResults != null &&
-                  applicationBloc.searchResults!.length != 0)
-                Container(
-                  height: (MediaQuery.of(context).size.height) * 0.6,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      color: Colors.black.withOpacity(0.6),
-                      backgroundBlendMode: BlendMode.darken),
-                ),
-              if (applicationBloc.searchResults != null &&
-                  applicationBloc.searchResults!.length != 0)
-                Container(
-                  height: (MediaQuery.of(context).size.height) * 0.6,
-                  child: ListView.builder(
-                    itemCount: applicationBloc.searchResults!.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text(
-                          applicationBloc
-                              .searchResults![index].description,
-                          style: TextStyle(color: Colors.white),
+
+    return GestureDetector(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: (applicationBloc.currentLocation == null)
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Column(
+                children: <Widget>[
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 10, top: 20, right: 10, bottom: 10),
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        hintText: 'Search Location',
+                        suffixIcon: Icon(Icons.search),
+                      ),
+                      onChanged: (value) => applicationBloc.searchPlaces(value),
+                    ),
+                  ),
+                  Stack(
+                    children: [
+                      Container(
+                        height: (MediaQuery.of(context).size.height) * 0.6,
+                        padding: const EdgeInsets.only(
+                            top: 10, left: 13, right: 13, bottom: 10),
+                        child: GoogleMap(
+                          myLocationEnabled: true,
+                          initialCameraPosition: CameraPosition(
+                              target: LatLng(
+                                  applicationBloc.currentLocation!.latitude,
+                                  applicationBloc.currentLocation!.longitude),
+                              zoom: 14),
+                          onMapCreated: (GoogleMapController controller) {
+                            _mapController.complete(controller);
+                          },
+                          markers: _markers,
                         ),
-                        onTap: () async {
-                          place = await PlaceService().getPlace(
-                              applicationBloc
-                                  .searchResults![index].placeId);
-                          moveToLocation();
-                          applicationBloc.searchResults = null;
-                        },
-                      );
-                    },
+                      ),
+                      if (applicationBloc.searchResults != null &&
+                          applicationBloc.searchResults!.isNotEmpty)
+                        Container(
+                          height: (MediaQuery.of(context).size.height) * 0.6,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              color: Colors.black.withOpacity(0.6),
+                              backgroundBlendMode: BlendMode.darken),
+                        ),
+                      if (applicationBloc.searchResults != null &&
+                          applicationBloc.searchResults!.isNotEmpty)
+                        SizedBox(
+                          height: (MediaQuery.of(context).size.height) * 0.6,
+                          child: ListView.builder(
+                            itemCount: applicationBloc.searchResults!.length,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                title: Text(
+                                  applicationBloc
+                                      .searchResults![index].description,
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                onTap: () async {
+                                  place = await PlaceService().getPlace(
+                                      applicationBloc
+                                          .searchResults![index].placeId);
+                                  moveToLocation();
+                                  applicationBloc.searchResults = null;
+                                  FocusManager.instance.primaryFocus?.unfocus();
+                                },
+                              );
+                            },
+                          ),
+                        )
+                    ],
                   ),
-                )
-            ],
-          ),
-          Container(
-            child: (place == null)
-                ? Column(
-              children: <Widget>[
-                Card(
-                  child: ListTile(
-                    title: Text('주소'),
+                  Container(
+                    child: (place == null)
+                        ? Column(
+                            children: const <Widget>[
+                              Card(
+                                child: ListTile(
+                                  title: Text('주소'),
+                                ),
+                              ),
+                              Card(
+                                child: ListTile(
+                                  title: Text('상세주소'),
+                                ),
+                              ),
+                            ],
+                          )
+                        : Column(
+                            children: <Widget>[
+                              Card(
+                                child: ListTile(
+                                  title: Text('${place!.name}'),
+                                ),
+                              ),
+                              Card(
+                                child: ListTile(
+                                  title: Text('${place!.formattedAddress}'),
+                                ),
+                              ),
+                            ],
+                          ),
                   ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('상세주소'),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        vertical: (MediaQuery.of(context).size.height) * 0.02,
+                        horizontal: (MediaQuery.of(context).size.width) * 0.3),
+                    child: TextButton(
+                      child: const Text('설정'),
+                      style: TextButton.styleFrom(
+                        primary: Colors.white,
+                        backgroundColor: Colors.blueAccent,
+                        onSurface: Colors.grey,
+                      ),
+                      onPressed: () {
+                        if (place == null) {
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: const Text("주의"),
+                                  content: const Text("위치를 선택해주세요"),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      child: Text("Close"),
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                    )
+                                  ],
+                                );
+                              });
+                        } else {
+                          print(place!.formattedAddress);
+                          Navigator.of(context).push(
+                              MaterialPageRoute(builder: (context) => Add()));
+                        }
+                      },
+                    ),
                   ),
-                ),
-              ],
-            )
-                : Column(
-              children: <Widget>[
-                Card(
-                  child: ListTile(
-                    title: Text('${place!.name}'),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text('${place!.formattedAddress}'),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(
-                vertical: (MediaQuery.of(context).size.height) * 0.02,
-                horizontal: (MediaQuery.of(context).size.width) * 0.3),
-            child: TextButton(
-              child: Text('설정'),
-              style: TextButton.styleFrom(
-                primary: Colors.white,
-                backgroundColor: Colors.blueAccent,
-                onSurface: Colors.grey,
+                ],
               ),
-              onPressed: () {
-                // 이전화면에 데이터 값(place값? or place.name, place.formatteAddress)
-              },
-            ),
-          ),
-        ],
       ),
     );
   }
@@ -185,24 +215,16 @@ class _MapSampleState extends State<MapSample> {
     final GoogleMapController controller = await _mapController.future;
     controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
         target:
-        LatLng(place.geometry!.location.lat, place.geometry!.location.lng),
-        zoom: 14.0)));
+            LatLng(place.geometry!.location.lat, place.geometry!.location.lng),
+        zoom: 16.0)));
   }
 
   void moveToLocation() async {
-    if (_markers.length > 0) {
+    if (_markers.isNotEmpty) {
       setState(() {
         _markers.clear();
       });
     }
-
-    GoogleMapController controller = await _mapController.future;
-    controller.animateCamera(
-      CameraUpdate.newLatLng(
-        LatLng(place!.lat, place!.lng),
-      ),
-    );
-
     setState(() {
       _markers.add(
         Marker(
@@ -215,27 +237,9 @@ class _MapSampleState extends State<MapSample> {
         ),
       );
     });
-  }
+    GoogleMapController controller = await _mapController.future;
 
-  Widget _showPlaceInfo() {
-    if (place == null) {
-      return Container();
-    }
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text('${place!.name}'),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('${place!.formattedAddress}'),
-            ),
-          ),
-        ],
-      ),
-    );
+    controller
+        .animateCamera(CameraUpdate.newLatLng(LatLng(place!.lat, place!.lng)));
   }
 }
